b5c9427808f07499541600eca1cb7d1ea08bff5f
Павел Ос <ospasha@mail.ru>
2022-04-06 16:01:26 +0300

    задача 2

--- 3-2.py
+++ 3-2.py
@@ -1,5 +1,5 @@
-number1 = int(input('Введите первое число '))
-number2 = int(input('Введите второе число '))
-number3 = int(input('Введите третье число '))
+number1 = float(input('Введите первое число '))
+number2 = float(input('Введите второе число '))
+number3 = float(input('Введите третье число '))
 decision = (number1+number2+number3)/3
 print(round(decision, 3))

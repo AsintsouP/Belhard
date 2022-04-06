673c8b2871c27f1663b7b3b2039cabb56ff7dfe0
Павел Ос <ospasha@mail.ru>
2022-04-06 16:21:40 +0300

    задача 3

---
+++ 3-3.py
@@ -0,0 +1,14 @@
+name = input('Введите имя ')
+age = input('Введите возраст ')
+city = input('Введите город ')
+print('Меня зовут %s. Мне %s лет. Я из города %s' %(name, age, city))
+
+name1 = input('Введите имя ')
+age1 = input('Введите возраст ')
+city1 = input('Введите город ')
+print('Меня зовут {}. Мне {} лет. Я из города {}'.format(name1, age1, city1))
+
+name2 = input('Введите имя ')
+age2 = input('Введите возраст ')
+city2 = input('Введите город ')
+print(f'Меня зовут {name2}. Мне {age2} лет. Я из города {city2}')

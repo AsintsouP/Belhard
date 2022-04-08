fb2e2b18f356bf45b33d1782ab30c636b93457fd
Павел Ос <ospasha@mail.ru>
2022-04-08 14:09:06 +0300

    задача 5

--- 4-4.py
+++ 4-4.py
@@ -1,4 +1,4 @@
-list_1 = {1, 2, 3}
+list_1 = {1, 2, 3, 8}
 list_2 ={3, 5, 1 ,8}
 general = list_1.intersection(list_2)
 print(sum(general))
---
+++ 4-5.py
@@ -0,0 +1,6 @@
+list_1 = [1, 2, 3, 1, 5, 5, 1]
+list_2 = set(list_1)
+number = len(list_1) - len(list_2)
+print(list_1)
+print(list_2)
+print(number)

d219382c140977d06fc18c7c194fc702c98acdb4
Павел Ос <ospasha@mail.ru>
2022-04-08 14:11:14 +0300

    задача 5

--- 4-4.py
+++ 4-4.py
@@ -1,4 +1,6 @@
-list_1 = {1, 2, 3, 8}
-list_2 ={3, 5, 1 ,8}
-general = list_1.intersection(list_2)
+list_1 = [1, 2, 3, 8]
+list_2 = [3, 5, 1, 8]
+list1 = set(list_1)
+list2 = set(list_2)
+general = list1.intersection(list2)
 print(sum(general))

9b48c439cb6acba630234648f27d1965abecdb76
Павел Ос <ospasha@mail.ru>
2022-04-12 15:41:43 +0300

    задача 3

--- 4-3.py
+++ 4-3.py
@@ -4,6 +4,6 @@ name = input('name ')
 mail = input('mail ')
 dict_1 = {name, mail}
 defdict = collections.defaultdict(list)
-for i in range(0, n):
+for i in range(0, n+1):
     defdict[i].append(dict_1)
 print(defdict)

f97ceccf59bd924e373baf0901248f6d02944885
Павел Ос <ospasha@mail.ru>
2022-04-12 21:28:18 +0300

    урок 5

--- new.py
+++ new.py
@@ -1,3 +1,25 @@
-text = input()
-a = text.find(' ')
-b = text.rfind(' ')
+# numbers = '4 5 6 7 4 5 6 7'
+# numbers = numbers.split(' ')
+# for i, number in enumerate(numbers):
+#     numbers[i] = int(number)
+# print(numbers)
+
+# text = input()
+# for i, elem in enumerate(text):
+#     if not i % 2:
+#         print(text)
+# for i in range(0, len(text), 2):
+#         print(text[i])
+
+
+
+reserved_number = 0
+tmp_original = number
+while tmp_original > 0:
+    reserved_number = (reserved_number * 10) + tmp_original % 10
+    tmp_original = tmp_original // 10
+
+if number == reserved_number:
+    print('Palindrome')
+else:
+    print('No Palindrome')
